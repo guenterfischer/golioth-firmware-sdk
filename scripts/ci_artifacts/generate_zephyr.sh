d45a9f0a8afbea97282f9e4415c22ccbd33b2252
Mike Szczys <mike@golioth.io>
2024-04-22 15:54:38 -0500

    Automatic CI artifact generation (updated for v0.16.0)
    See CI_ARTIFACT_README.md for instructions on how to use this branch
    
    Updated to Golioth Firmware SDK v0.16.0
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

---
+++ scripts/ci_artifacts/generate_zephyr.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+declare -a zephyr_boards
+# format: 'west_board_name'
+zephyr_boards[0]='esp32_devkitc_wrover/esp32/procpu'
+zephyr_boards[1]='mimxrt1024_evk'
+zephyr_boards[2]='nrf52840dk/nrf52840'
+zephyr_boards[3]='rak5010/nrf52840'
+
+declare -a ncs_boards
+# format: 'west_board_name'
+ncs_boards[0]='nrf9160dk/nrf9160/ns'
+
+if [ "${PWD##*/}" != "golioth-firmware-sdk" ]; then
+    echo "You must run from the Golioth SDK root directory: golioth-firwmare-sdk"
+    exit -1
+fi
+
+rm -rf ci_zephyr_artifacts
+mkdir ci_zephyr_artifacts
+
+### NCS
+python -m west config manifest.file west-ncs.yml
+python -m west update
+
+for ncs_board in "${ncs_boards[@]}"
+do
+    rm -rf build
+    python -m west build -b ${ncs_board} --sysbuild examples/zephyr/fw_update
+    mv build/fw_update/zephyr/zephyr.signed.bin ci_zephyr_artifacts/ncs-${ncs_board//\//_}.bin
+    python -m west build -b ${ncs_board} --sysbuild examples/zephyr/fw_update -- -DCONFIG_GOLIOTH_COAP_HOST_URI=\"coaps://coap.golioth.dev\"
+    mv build/fw_update/zephyr/zephyr.signed.bin ci_zephyr_artifacts/ncs_devserver-${ncs_board//\//_}.bin
+done
+
+### Zephyr
+python -m west config manifest.file west-zephyr.yml
+python -m west update
+python -m west blobs fetch hal_espressif
+
+for zephyr_board in "${zephyr_boards[@]}"
+do
+    rm -rf build
+    python -m west build -b ${zephyr_board} --sysbuild examples/zephyr/fw_update
+    mv build/fw_update/zephyr/zephyr.signed.bin ci_zephyr_artifacts/zephyr-${zephyr_board//\//_}.bin
+    python -m west build -b ${zephyr_board} --sysbuild examples/zephyr/fw_update -- -DCONFIG_GOLIOTH_COAP_HOST_URI=\"coaps://coap.golioth.dev\"
+    mv build/fw_update/zephyr/zephyr.signed.bin ci_zephyr_artifacts/zephyr_devserver-${zephyr_board//\//_}.bin
+done
